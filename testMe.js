6842f2fc376143f95d4f9cbb32ffe41103ea80ce
John Rucker <John.Rucker@Solar-Current.com>
2018-09-12 07:59:56 -0500

    add ability to pass address to testMe

--- testMe.js
+++ testMe.js
@@ -1,27 +1,34 @@
 var irTransmitter =     require('./irdClass.js');
 var package =           require('./package.json');
 
-const deviceAddress = 170;                                          // 170 = broadcast address (all gauges withing range will respond)
+var deviceAddress = 170;                                          // 170 = broadcast address (all gauges withing range will respond)
 const calibration = [[0,0],[25,155],[50,310],[75,460],[100,620]];   // This is a generic calibration table from 0 to 100
 
+if(process.argv.length == 3){
+    var deviceAddress = process.argv[2];
 
+    console.log('device address ovride on command line to:' + deviceAddress);
+
+}
+
+console.log('Creating irdClass for device address: '+ deviceAddress);
 var irTX = new irTransmitter(deviceAddress, calibration);
 
 
 var pVer = package.version;
 console.log('class ver = '+ pVer);
 
-console.log('adding value of 50 to command queue to be sent to gauge with the address of ' + deviceAddress);
+console.log('adding value of 50% to command queue to be sent to gauge with the address of ' + deviceAddress);
 irTX.sendValue(50);
-var valToSend = 1;
+var valToSend = 0;
 setInterval(function(){
     if(valToSend == 100){
         valToSend = 1;
     } else {
         valToSend++
     }
-    console.log('Adding value of' + valToSend + ' to command queue to be sent to gauge with the address of ' + deviceAddress);
+    console.log('Adding value of' + valToSend + '% to command queue to be sent to gauge with the address of ' + deviceAddress);
     irTX.sendValue(valToSend);
-}, 600000);
+}, 300000);
 
 
